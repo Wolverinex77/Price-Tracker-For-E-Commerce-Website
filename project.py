619a1e07fe297f15a770c341e00d52a44bbf255a
Wahib Naeem <121589993+Wolverinex77@users.noreply.github.com>
2025-09-19 15:17:06 +0500

    Update project.py

--- project.py
+++ project.py
@@ -3,32 +3,33 @@ from display import show_main_menu
 from mail import mail
 from categories import get_categories
 
+
+def run_search():
+    search_data()
+
+
+def run_categories():
+    get_categories()
+
+
+def run_mail():
+    mail()
+
+
 def main():
     while True:
-        user_input=show_main_menu()
+        user_input = show_main_menu()
 
-        if user_input=="1":
-            search_data()
-         
-        elif user_input=="2":
-            get_categories()
-            
-        elif user_input=="3":
-            mail()
-            
-            
-        elif user_input=="4":
+        if user_input == "1":
+            run_search()
+        elif user_input == "2":
+            run_categories()
+        elif user_input == "3":
+            run_mail()
+        elif user_input == "4":
             print("Good bye 😊")
             exit()
-    
-        
-        
+
+
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
